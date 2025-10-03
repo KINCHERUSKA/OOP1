ccddf913333a6e80c29f10c8205ebf693a8f04c1
KINCHERUSKA <kryovv718@gmail.com>
2025-10-03 17:47:20 +0300

    Functons templates

--- OOP1/OOP1.cpp
+++ OOP1/OOP1.cpp
@@ -8,6 +8,7 @@
 #include "Point3D.h"
 #include "Fraction.h"
 #include "Point.h"
+#include "Templates.h"
 
 using namespace std;
 
@@ -17,29 +18,21 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    Point p1{ 5,5 };
-    Point p2{ 10,10 };
+    int intArray[]{ 1,3,7,-4,-2,4 };
+    int size = sizeof(intArray) / sizeof(int);
+    cout << "Оригинальный массив" << endl;
+    display(intArray, size);
+    sort(intArray, size);
+    cout << "Сортированый массив" << endl;
+    display(intArray, size);
 
-    cout << "Дистанция между p1: " << p1 << " и p2: " << p2 << " составляет " << distance(p1, p2) << endl;
-
-    const int testCases{ 9 };
-    Point points[testCases]{
-        {0,0},
-        {1,1},
-        {-1,1},
-        {-1,-1},
-        {1,-1},
-        {1,0},
-        {-1,0},
-        {0,1},
-        {0,-1},
-    };
-
-    for (int i{ 0 }; i < testCases; i++) {
-        cout << points[i] << ' ';
-        quadrantDecode(quadrant(points[i]));
-        cout << endl;
-    }
+    char charArray[]{ 'd','s','w','g','h'};
+    size = sizeof(charArray) / sizeof(char);
+    cout << "Оригинальный массив" << endl;
+    display<char>(charArray, size);
+    sort<char>(charArray, size);
+    cout << "Сортированый массив" << endl;
+    display<char>(charArray, size);
 
     return 0;
 }
---
+++ OOP1/Templates.h
@@ -0,0 +1,20 @@
+#pragma once
+
+template<class T>
+void sort(T array[], size_t size) {
+    for (int k = size - 1; k > 0; k--) {
+        for (int j = 0; j < k; j++) {
+            if (array[j] > array[j + 1]) {
+                swap(array[j], array[j + 1]);
+            }
+        }
+    }
+}
+
+template<class T>
+void display(T array[], size_t size) {
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
